63936c76979288e70d883e7d87133c04a924e7de
Willian Dias <willianyep@gmail.com>
2025-02-27 12:28:49 -0300

    projeto inicial

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//armazenar lista do amigo secreto
+let amigos = [];
+let numeroLimite = 30;
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) 
+{
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+
+}
+function exibirMensagemInicial() 
+{
+    exibirTextoNaTela('h1', 'Amigo Secreto');
+    exibirTextoNaTela('h2', 'Gigite Nome e Sobrenome');
+
+}
+
+exibirMensagemInicial();
+
+function verificarNome () 
+{
+    let Nome = document.querySelector('input').value;
+}
